bd9cccbce05299d3623261078a5860078c780b7d
mamdouh <mamdouh>
2022-01-21 11:51:16 +0200

    jumia admin dashboard with angular

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  // APIURL: "http://localhost:5000/api/v1",
+  // APIURL: "https://jumia-apis.herokuapp.com/api/v1",
+  //APIURL: "https://jumaiclone.herokuapp.com/api/v1",
+  APIURL:"https://jumia-simiulation-site.herokuapp.com/api/v1"
+};
